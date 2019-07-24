0031e2b0219fcab2e74b7f72492700869677c444
Deepika1812 <deepikasiddunuri1812@gmail.com>
2019-07-24 10:23:36 +0530

    added a line

--- testcase1.py
+++ testcase1.py
@@ -1,10 +1,5 @@
-<<<<<<< practise
-=======
-<<<<<<< HEAD
-=======
+
 import unittest
->>>>>>> 87693134eda0d36aa23e54ccf26948d63fecce16
->>>>>>> master
 def add(num1,num2):
 	return num1+num2
 def mul(num1,num2):
@@ -17,9 +12,5 @@ def test_mul():
 	assert mul(3,5)==12
 def test_sub():
 	assert sub(3,4)==0
-<<<<<<< practise
 def test():
-	assert True
-=======
-
->>>>>>> master
+	assert False
